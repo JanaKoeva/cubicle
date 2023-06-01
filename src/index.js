f74b5eeb2f56fb85018f6e2ad609a2dea9457cbc
Jana Koeva <jana.koeva@outlook.com>
2023-06-01 06:43:12 +0300

    Add express  and handlebars config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const express=require('express');
-const handlebars=require('express-handlebars');
-const path=require('path')
+
+
+const handlebarsConfigurator=require('./config/handlebarsConfig')
+const expressConfigurator=require('./config/expressConfig')
 
 const app=express();
 const PORT=5000;
 
-//Express config
-app.use(express.static(path.resolve(__dirname,'public')))
+ //Express config(app)
+expressConfigurator(app);
+// vmesto red 11 i red 5-require('./config/expressConfig')(app)
 
-//Handlebars config
-app.engine('hbs', handlebars.engine({
-    extname:'hbs',
-    
-}))
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
+//Handlebars config/app/
+handlebarsConfigurator(app);
 
 
 //Routes
