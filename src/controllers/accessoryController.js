ffe67a2650e1a173ec8ebb73198746ae5af6f719
Jana Koeva <jana.koeva@outlook.com>
2023-06-04 23:46:40 +0300

    Add create accessory page

---
+++ src/controllers/accessoryController.js
@@ -0,0 +1,13 @@
+const router=require('express').Router();
+
+router.get('/create',(req,res)=>{
+    res.render('accessory/create')
+})
+
+router.post('/create',(req,res)=>{
+    const body=req.body;
+    
+
+    res.redirect('/');
+})
+module.exports=router;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const router=require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubesController');
+const accessoryController=require('./controllers/accessoryController')
 
 //Routes
 //app.get('/', homeController.getHome);
 router.use(homeController);
 router.use('/cubes', cubeController);
+router.use('/accessories', accessoryController);
 router.get('*', (req, res) => {
     res.redirect('/404')
 });
